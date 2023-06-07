9c864d121bc66a186b3f0fc60293a834b887318e
Evans-Gash <evansgash88@gmail.com>
2023-06-07 06:05:25 -0700

    Done and Dusted

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b, c):
+         """Match the bytecode sample in the task."""
+    if a < b:
+        return c
+    elif c > b:
+        return a + b
+    else:
+        return a * b - c
