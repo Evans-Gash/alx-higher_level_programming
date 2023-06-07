0b95ecebe7b9a2de7ac88f90f55f7fe9b5b8f251
Evans-Gash <evansgash88@gmail.com>
2023-06-07 01:06:57 -0700

    python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""Print the alphabets in small letters, there shouldn't be a new line after."""
+""Print the alphabets in small letters, there shouldn't be a new line after.""
+
+
 def print_alphabet():
     for letter in range(97, 123):
         print("{}".format(chr(letter)), end="")
+
 
 if __name__ == "__main__":
     print_alphabet()
