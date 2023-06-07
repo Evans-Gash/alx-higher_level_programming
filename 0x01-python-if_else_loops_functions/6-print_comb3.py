f87b46f8f9376e045529f0425fc1518a90debcaf
Evans-Gash <evansgash88@gmail.com>
2023-06-07 02:05:39 -0700

    New

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_combinations():
+    for digit1 in range(0, 9):
+        for digit2 in range(digit1 + 1, 10):
+            print("{}{}".format(digit1, digit2), end=", ")
+    print("89")
+
+if __name__ == "__main__":
+    print_combinations()
