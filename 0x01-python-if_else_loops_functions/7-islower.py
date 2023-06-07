41481cf0ce5a245bc93e13cea7fd3c4a5516a977
Evans-Gash <evansgash88@gmail.com>
2023-06-07 02:53:40 -0700

    new

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,10 +1,8 @@
-#!/usr/bin/python3def islower(c):
-
-"""scan for small letters."""
+#!/usr/bin/python3
 
 def is_lowercase(c):
     return 97 <= ord(c) <= 122
 
 if __name__ == "__main__":
     c = input("Enter a character: ")
-    print(is_lowercase(c))    
+    print(is_lowercase(c))
