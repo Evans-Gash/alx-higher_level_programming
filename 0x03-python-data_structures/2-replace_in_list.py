f8dc0da8dd67cf0a35c8a0d6fe1c93baa5887860
Evans-Gash <evansgash88@gmail.com>
2023-06-12 06:12:23 -0700

    python-data_structures

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,16 +4,13 @@
 def replace_in_list(evans_list, idx, element):
     """Replace an element of a list at a specific position."""
     if idx >= 0 and idx < len(evans_list):
-        new_list = evans_list.copy()
-        new_list[idx] = element
-        return new_list
-    else:
-        return evans_list
+        evans_list[idx] = element
+    return evans_list
 
 
 if __name__ == "__main__":
-    gash_list = [1, 2, 3, 4, 5]
+    evans_list = [1, 2, 3, 4, 5]
     index = 2
     new_element = 7
-    result = replace_in_list(gash_list, index, new_element)
+    result = replace_in_list(evans_list, index, new_element)
     print(result)
