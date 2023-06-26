10386bf80cd97f7d1e68d6de116a7f88474ffbdb
Evans-Gash <evansgash88@gmail.com>
2023-06-26 02:22:02 -0700

    Quantum

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
 
-def print_integer_elements(gash_list=[], x=0):
-    """Print the first x integer elements from a list.
-
-    Args:
-        gash_list (list): The list to print elements from.
-        x (int): The number of integer elements from gash_list to print.
-
-    Returns:
-        The number of integer elements printed.
-    """
-    count = 0
-    for element in gash_list[:x]:
-        if isinstance(element, int):
-            print(str(element), end="")
-            count += 1
+def safe_print_list_integers(my_list=[], x=0):
+    gash = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            gash += 1
+        except (ValueError, TypeError):
+            continue
     print("")
-    return (count)
+    return (gash)
