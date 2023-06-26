a1545315cbe91b240fa4daeb65958d75823937ba
Evans-Gash <evansgash88@gmail.com>
2023-06-26 03:17:34 -0700

    Quantum

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -2,14 +2,13 @@
 
 def magic_calculation(a, b):
 
-    result = (Zero)
-
+    result = 0
     for i in range(1, 3):
         try:
             if i > a:
                 raise Exception('Too far')
             result += a ** b / i
         except Exception:
-            result = (b + a)
+            result = b + a
             break
     return (result)
