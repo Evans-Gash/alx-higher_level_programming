f742fc63fd569c88bfb40aa36664128024892c85
Evans-Gash <evansgash88@gmail.com>
2023-06-27 02:00:58 -0700

    Python Classes

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+""" Create a class named Square that explains a square by: (based on 1-square.py)."""
+
+
+class Square:
+    """Reps a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+        Args:
+            size (integer): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
