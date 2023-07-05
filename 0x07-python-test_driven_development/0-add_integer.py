6562b3136ce770fb9fd38e9d6cbfb1d0812ce28d
Evans-Gash <evansgash88@gmail.com>
2023-07-05 03:04:02 -0700

    We Gon Be Alright

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,10 @@
-#!/usr/bin/python3
-
 def add_integer(a, b=98):
-    """A function that adds 2 integers.
+    """A function that adds two integers.
     Return the integer addition of a and b.
-    Float arguments are casted to integers before addition is performed.
+    Float arguments are cast to integers before addition is performed.
     Raises:
         TypeError: If either a or b is a non-integer and non-float.
     """
-
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer or float")
     if not isinstance(b, (int, float)):
