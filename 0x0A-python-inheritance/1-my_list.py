f82f09e05bd44659788deaa3afe08d40114e0db1
Evans-Gash <evansgash88@gmail.com>
2023-07-10 04:43:02 -0700

    ElChapo-jr

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+
 """A class that inherits from the parent class"""
+class MyList(list):
+    """A class that inherits from the parent class"""
 
-class CustomList(list):
-"""A class that inherits from the parent class"""
-
-def print_sorted(self):
-    """Prints a list in sorted ascending order."""
-    print(sorted(self))
-
+    
+    def print_sorted(self):
+        """Prints a list in sorted ascending order."""
+        print(sorted(self))
