5b74b7a9d73432b5ed8cfa5bf27ddcc9eb368595
Evans-Gash <evansgash88@gmail.com>
2023-08-16 04:55:48 -0700

    More Queries SQL

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
---shows all the privileges/rights for user _0d_1 & _0d_2
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- privileges for user_0d_1
+SHOW GRANTS FOR user_0d_1@localhost;
+-- privileges for user_0d_2
+SHOW GRANTS FOR user_0d_2@localhost;
