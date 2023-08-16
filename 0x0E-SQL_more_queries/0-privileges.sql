0e8daa931e747bd379e369c9e38c8bd8cc4af1e3
Evans-Gash <evansgash88@gmail.com>
2023-08-16 04:53:03 -0700

    More Queries SQL

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+--shows all the privileges/rights for user _0d_1 & _0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- will create a brand new user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY "user_0d_1_pwd";
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
