f26660ce1f18aba73e80169f2d10b296d88aebb6
Evans-Gash <evansgash88@gmail.com>
2023-08-16 06:29:17 -0700

    More Queries SQL

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,10 +1,5 @@
--- Lists all cities of CA in the database hbtn_0d_usa.
--- Results are ordered by ascending cities.id.
-SELECT `id`, `name`
-  FROM `cities`
- WHERE `state_id` IN
-       (SELECT `id`
-	        FROM `states`
-	       WHERE `name` = "California")
-ORDER BY `id`;
- 7 changes: 7 additions & 0 deletions7 
+SELECT id, name FROM cities
+WHERE state_id = (
+      SELECT id FROM states
+      WHERE name = "California")
+ORDER BY id;
